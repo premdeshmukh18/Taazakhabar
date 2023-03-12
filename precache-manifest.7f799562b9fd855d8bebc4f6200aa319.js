0122f8cb4ab31464077f20f7420eca542d2f38ad
Prem Deshmukh <Premdeshmukh1818@gmail.com>
2023-03-12 22:59:44 +0530

    code

---
+++ precache-manifest.7f799562b9fd855d8bebc4f6200aa319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbf2fb27a5466ef2afb2",
+    "url": "/static/js/main.dbf2fb27.chunk.js"
+  },
+  {
+    "revision": "205849d973eaa941ab0f",
+    "url": "/static/js/1.205849d9.chunk.js"
+  },
+  {
+    "revision": "eaa14a68e7ede818b5eb",
+    "url": "/static/js/2.eaa14a68.chunk.js"
+  },
+  {
+    "revision": "1ed698c15cd075133fed",
+    "url": "/static/js/runtime~main.1ed698c1.js"
+  },
+  {
+    "revision": "0c87b5a6e4fb5548a5dcf71f1bb7e82b",
+    "url": "/static/media/loading.0c87b5a6.gif"
+  },
+  {
+    "revision": "51efd8255cee19a5d604b552a86698e2",
+    "url": "/index.html"
+  }
+];
